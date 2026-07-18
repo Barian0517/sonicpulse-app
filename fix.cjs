6aa4a46e275bbd0c67ccc0d1216db080f1a492ac
Barian0517 <barian0517@gmail.com>
2026-07-18 14:15:17 +0800

    整理專案資料夾

--- fix.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('providers/MusicFreeProvider.ts', 'utf8');
-const replacement = '`http://${window.location.hostname === \\'tauri.localhost\\' ? \\'127.0.0.1\\' : window.location.hostname}:30001';
-c = c.replace(/'http:\/\/:30001/g, replacement);
-c = c.replace(/`http:\/\/:30001/g, replacement);
-fs.writeFileSync('providers/MusicFreeProvider.ts', c);
--- fix_musicfree.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-let c = fs.readFileSync('providers/MusicFreeProvider.ts', 'utf8');
-
-c = c.replace(/'http:\/\/:30001([^']*)'/g, "`http://${window.location.hostname === 'tauri.localhost' ? '127.0.0.1' : window.location.hostname}:30001$1`");
-
-c = c.replace(/`http:\/\/:30001([^`]*)`/g, "`http://${window.location.hostname === 'tauri.localhost' ? '127.0.0.1' : window.location.hostname}:30001$1`");
-
-fs.writeFileSync('providers/MusicFreeProvider.ts', c);
